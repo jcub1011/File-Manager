ba6c8989be65624965d457d17e8abed4a8cb1a81
Jacob McCormack <91000914+jcub1011@users.noreply.github.com>
2026-06-29 11:38:57 -0400

    Set AOT compatibility.

---
+++ src/FileManager.Core/Profiles/Profile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileManager.Core.Profiles;
+
+public sealed record Profile
+{
+    /// <summary>
+    /// The version of this schema.
+    /// </summary>
+    public required int SchemaVersion { get; init; }
+
+    /// <summary>
+    /// The unique identifier of this profile.
+    /// </summary>
+    public required Guid Id { get; init; }
+
+    /// <summary>
+    /// The name of this profile.
+    /// </summary>
+    public required string Name { get; init; }
+}
