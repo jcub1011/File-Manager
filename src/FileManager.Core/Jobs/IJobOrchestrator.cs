fd34e765f66acfa2e37d2a9d82d92c81d3439161
Jacob McCormack <91000914+jcub1011@users.noreply.github.com>
2026-07-06 15:11:17 -0400

    Added job services.

---
+++ src/FileManager.Core/Jobs/IJobOrchestrator.cs
@@ -0,0 +1,12 @@
+﻿using FileManager.Contracts.IPC;
+using FileManager.Core.Primitives;
+using System.Threading.Tasks;
+
+namespace FileManager.Core.Jobs;
+
+public interface IJobOrchestrator
+{
+    Result Start();
+    Task StopAsync();
+    EngineStatusSnapshot GetStatus();
+}
